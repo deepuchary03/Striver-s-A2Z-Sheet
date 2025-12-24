ec45c58b5b53652b2f738912be6432b3cbd6b00d
deepuchary03 <deepuchary03@gmail.com>
2025-12-24 20:20:43 +0530

    pattern

---
+++ Basics/Pattern6.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class Pattern6 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                System.out.print(j+1+" ");
+            }System.out.println();
+        }
+    }
+}
---
+++ Basics/Pattern9.java
@@ -0,0 +1,27 @@
+package Basics;
+
+public class Pattern9 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int j=i;j<n-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
