cd89156c1b2a64689afe12f88b5c1a54a0f1852b
deepuchary03 <deepuchary03@gmail.com>
2025-12-25 13:18:30 +0530

    patterns over

---
+++ Basics/Pattern22.java
@@ -0,0 +1,19 @@
+package Basics;
+
+public class Pattern22 {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=0;i<2*n-1;i++){
+            for(int j=0;j<2*n-1;j++){
+            int top=i;
+            int left=j;
+            int right=(2*n-2)-j;
+            int down=(2*n-2)-i;
+            int val=Math.min(Math.min(top,down),Math.min(left,right));
+            System.out.print(n-val);
+             }
+             System.out.println();
+            }
+
+    }
+}
