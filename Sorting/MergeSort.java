838c229479185f45d22b3a9f6de40fc2c72c6890
deepuchary03 <deepuchary03@gmail.com>
2025-12-25 20:18:21 +0530

    mergeSort

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -1,7 +1,43 @@
 package Sorting;
+import java.util.ArrayList;
+// Input: nums = [7, 4, 1, 5, 3]
+
+// Output: [1, 3, 4, 5, 7]
 
 public class MergeSort {
+    public static void merge(int nums[],int low,int mid,int high){
+        ArrayList<Integer> ans=new ArrayList<>();
+        int left=low;
+        int right=mid+1;
+        while(left<=mid && right<=high){
+            if(nums[left]>=nums[right]){
+                ans.add(nums[right]);
+                right++;
+            }else{
+                ans.add(nums[left]);
+                left++;
+            }
+        }
+        while(left<=mid){
+            ans.add(nums[left++]);
+        }
+        while(right<=high){
+            ans.add(nums[right++]);
+        }
+        for(int i=low;i<=high;i++) nums[i]=ans.get(i-low);
+
+    }
+
+    public static void mergeSort(int[] nums,int low,int high){
+        if(low>=high)return;
+        int mid=(low+high)/2;
+        mergeSort(nums,low,mid);
+        mergeSort(nums,mid+1,high);
+        merge(nums,low,mid,high);
+    }
     public static void main(String[] args) {
-        
+        int nums[]={7, 4, 1, 5, 3};
+        mergeSort(nums,0,nums.length-1);
+        for(int i:nums) System.out.print(i+" ");
     }
 }
