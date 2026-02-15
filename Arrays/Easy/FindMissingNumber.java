2e0423f9ed52c2cdbd3d4bc3462134286f747efe
deepuchary03 <deepuchary03@gmail.com>
2026-02-15 19:01:01 +0530

    optimal approach

--- Arrays/Easy/FindMissingNumber.java
+++ Arrays/Easy/FindMissingNumber.java
@@ -1,11 +1,15 @@
 package Arrays.Easy;
 
 public class FindMissingNumber {
-    public static void main(String[] args) {
-        int[] nums={0, 2, 3, 1, 4};
+    public static int missing(int[] nums){
         int ans=0;
         for(int i:nums) ans=ans^i;
-        for(int i=0;i<=nums.length;i++) ans=ans^i;
+        for(int i=1;i<=nums.length;i++) ans=ans^i;
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] nums={0, 2, 3, 1, 4};
+        int ans=missing(nums);
         System.out.println(ans);
     }
 }
--- Arrays/Easy/LeftRotateByKplaces.java
+++ Arrays/Easy/LeftRotateByKplaces.java
@@ -1,20 +1,29 @@
 package Arrays.Easy;
 
 public class LeftRotateByKplaces {
+    public static void reverse(int[] nums,int l,int r){
+        while(l<r){
+            int temp=nums[l];
+            nums[l]=nums[r];
+            nums[r]=temp;
+            l++;r--;
+        }
+    }
+    public static void rotate(int[] nums, int k) {
+        int n=nums.length;
+        k=k%n;
+        
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
+
+    }
     public static void main(String[] args) {
         int nums[]={1, 2, 3, 4, 5, 6};
         int k=2;
 
-        int n=nums.length;
-        int d=k%n;
-        int ans[]=new int[n];
-        for(int i=0;i<n-d;i++){
-            ans[i]=nums[i+d];
-        }
-        for(int i=0;i<d;i++){
-            ans[n-d+i]=nums[i];
-        }
-        for(int i:ans){
+        rotate(nums, k);
+        for(int i:nums){
             System.out.print(i+" ");
         }
 
