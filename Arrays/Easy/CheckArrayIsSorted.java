d4af423c4aa6f8c77fd5af1a11e345408b0eb3fe
deepuchary03 <deepuchary03@gmail.com>
2026-02-15 18:41:33 +0530

    revise1

--- Arrays/Easy/CheckArrayIsSorted.java
+++ Arrays/Easy/CheckArrayIsSorted.java
@@ -1,15 +1,16 @@
 package Arrays.Easy;
 
 public class CheckArrayIsSorted {
+    public static boolean isSorted(int[] arr){
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]) return false;
+        }
+        return true;
+    }
     public static void main(String[] args) {
         int arr[]={1, 2, 3, 4, 5};
-        boolean ans=true;
-        for(int i=0;i<arr.length-1;i++){
-            if(arr[i]>arr[i+1]){
-                ans=false;
-                break;
-            }
-        }
+        boolean ans=isSorted(arr);
+        
         System.out.println(ans);
     }
 }
