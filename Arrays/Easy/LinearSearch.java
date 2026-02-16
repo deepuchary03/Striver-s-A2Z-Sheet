f669002cd1ae69b05aeada3c0508ca3f66c4e7a9
deepuchary03 <deepuchary03@gmail.com>
2026-02-16 17:27:47 +0530

    update

--- Arrays/Easy/LinearSearch.java
+++ Arrays/Easy/LinearSearch.java
@@ -1,15 +1,14 @@
 package Arrays.Easy;
 
 public class LinearSearch {
+    public static int linearSearch(int[] arr,int t){
+        for(int i=0;i<arr.length;i++) if(arr[i]==t) return i;
+        return -1;
+    }
     public static void main(String[] args) {
         int[] nums={2, 3, 4, 5, 3};
-        int target=3;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                System.out.println("Found at index:"+i);
-                return;
-            }
-        }
-        System.out.println(-1);
+        int target=5;
+        int ans=linearSearch(nums, target);
+        System.out.println(ans);
     }
 }
