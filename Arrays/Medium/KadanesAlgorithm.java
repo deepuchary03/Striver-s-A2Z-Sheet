3b6194e34cb0ad4c0b078129371c4efb16c53de4
deepuchary03 <deepuchary03@gmail.com>
2026-02-17 19:07:35 +0530

    .. ..

--- Arrays/Medium/KadanesAlgorithm.java
+++ Arrays/Medium/KadanesAlgorithm.java
@@ -5,16 +5,19 @@ package Arrays.Medium;
 // Output: -2
 
 public class KadanesAlgorithm {
+    public static int kadanesAlgorithm(int[] nums){
+        int ans=Integer.MIN_VALUE;
+        int temp=0;
+        for(int i:nums){
+            temp=temp+i;
+            ans=Math.max(ans,temp);
+            if(temp<0) temp=0;
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         int[] nums={-2, -3, -7, -2, -10, -4};
 
-        int ans=Integer.MIN_VALUE,temp=0;
-        for(int i:nums)
-            {
-                temp=temp+i;
-                ans=Math.max(ans,temp);
-                if(temp<0) temp=0;
-            }
-            System.out.println(ans);
+            System.out.println(kadanesAlgorithm(nums));
     }
 }
