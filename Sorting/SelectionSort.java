7b07efa9ef3bf6066ff12655591b2e67136bef64
deepuchary03 <deepuchary03@gmail.com>
2026-02-18 19:08:48 +0530

    aa

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -2,10 +2,7 @@ package Sorting;
 // Input: nums = [7, 4, 1, 5, 3]
 
 // Output: [1, 3, 4, 5, 7]
-
 // Explanation: 1 <= 3 <= 4 <= 5 <= 7.
-
-
 public class SelectionSort {
     public static void selectionSort(int[] arr){
         for(int i=0;i<arr.length;i++){
