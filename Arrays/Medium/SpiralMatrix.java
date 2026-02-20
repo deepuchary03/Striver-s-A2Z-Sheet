c60fb4bbe36c7cf9108b23521be7798fd78c1aee
deepuchary03 <deepuchary03@gmail.com>
2026-02-20 20:31:21 +0530

    aa

--- Arrays/Medium/SpiralMatrix.java
+++ Arrays/Medium/SpiralMatrix.java
@@ -2,7 +2,6 @@ package Arrays.Medium;
 // Input: matrix = [[1, 2, 3], 
 //                  [4 ,5 ,6], 
 //                  [7, 8, 9]]
-
 // Output: [1, 2, 3, 6, 9, 8, 7, 4, 5]
 import java.util.ArrayList;
 
