fcb91e356a51049c795ab6cc2cf9c46889caa3b8
deepuchary03 <deepuchary03@gmail.com>
2026-02-21 20:26:55 +0530

    Strings Easy

---
+++ Strings/Easy/RemoveOuterParenthesis.java
@@ -0,0 +1,22 @@
+package Strings.Easy;
+// Input: s = "(()())(())"
+// Output: "()()()"
+public class RemoveOuterParenthesis {
+
+    public static String remove(String s){
+        StringBuilder sb=new StringBuilder();
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==')') count--;
+            if(count!=0) sb.append(s.charAt(i));
+            if(s.charAt(i)=='(') count++;
+        }
+        return sb.toString();
+
+    }
+    public static void main(String[] args) {
+        String s="(()())(())";
+        System.out.println(remove(s));
+    }
+
+}
