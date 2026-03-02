058125db361a745d84094ecaba4d98ff694e172d
deepuchary03 <deepuchary03@gmail.com>
2026-03-02 19:42:45 +0530

    aa

--- Basics/Pattern2.java
+++ Basics/Pattern2.java
@@ -6,6 +6,8 @@ public class Pattern2 {
         for(int i=0;i<n;i++){
             for(int j=0;j<=i;j++){
                 System.out.print("* ");
+
+                
             }System.out.println();
         }
     }
