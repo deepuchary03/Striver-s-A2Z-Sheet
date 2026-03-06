1d8d7f393dbde1af7c2df919da64b8c1896ca963
deepuchary03 <deepuchary03@gmail.com>
2026-03-06 20:23:53 +0530

    ..

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -6,8 +6,7 @@ package Sorting;
 public class BubbleSort {
     public static void bubbleSort(int[] arr){
         for(int i=0;i<arr.length;i++)
-            {
-                for(int j=0;j<arr.length-i-1;j++){
+            { for(int j=0;j<arr.length-i-1;j++){
                     if(arr[j]>arr[j+1]){
                         int temp=arr[j+1];
                         arr[j+1]=arr[j];
