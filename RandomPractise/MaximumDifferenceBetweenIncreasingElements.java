458894dadf9891b8663c5fc881a164967c148e67
deepuchary03 <deepuchary03@gmail.com>
2026-03-10 18:39:04 +0530

    max diff

---
+++ RandomPractise/MaximumDifferenceBetweenIncreasingElements.java
@@ -0,0 +1,16 @@
+package RandomPractise;
+// input: nums = [7,1,5,4]
+// Output: 4
+
+public class MaximumDifferenceBetweenIncreasingElements {
+    public static void main(String args[]){
+        int[] nums={7,1,5,4};
+        int ans=-1;
+        int left=nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(left<nums[i]) ans=Math.max(ans,nums[i]-left);
+            left=Math.min(left,nums[i]);
+        }
+        System.out.println(ans);
+    }
+}
