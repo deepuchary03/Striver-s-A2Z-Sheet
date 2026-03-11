4a0beec24606b31e70fa0b557749a8c952bf2a20
deepuchary03 <deepuchary03@gmail.com>
2026-03-11 12:22:18 +0530

    set mismatch

---
+++ RandomPractise/DecodeTheMessage.java
@@ -0,0 +1,25 @@
+package RandomPractise;
+
+import java.util.HashMap;
+
+public class DecodeTheMessage {
+    public static void main(String s[]){
+ String key = "the quick brown fox jumps over the lazy dog", message = "vkbs bs t suepuv";
+        int temp=97;
+        HashMap<Character,Character> hm=new HashMap<>();
+        for(char c:key.toCharArray()){
+            if(c!=' '&&!hm.containsKey(c)){
+                hm.put(c,(char) temp);
+                temp++;
+            }
+
+        }
+        for(char c:hm.keySet()) System.out.println(c+" -> "+hm.get(c));
+        StringBuilder ans=new StringBuilder();
+        for(char c:message.toCharArray()){
+            if(c==' ') ans.append(" ");
+            else ans.append(hm.get(c));
+        }
+       System.out.println(ans.toString());
+    }
+}
---
+++ RandomPractise/SetMismatch.java
@@ -0,0 +1,28 @@
+package RandomPractise;
+// Input: nums = [1,2,2,4]
+// Output: [2,3]
+import java.util.*;
+public class SetMismatch {
+    public static void main(String[] args) {
+        int nums[]={1,2,2,4};
+        HashSet<Integer> hs=new HashSet<>();
+        int ans[]={-1,-1};
+        for(int i:nums){
+            if(hs.contains(i)){
+                ans[0]=i;
+            }else{
+                hs.add(i);
+            }
+
+        }
+        for(int i=1;i<=nums.length;i++){
+            if(!hs.contains(i)) {
+                ans[1]=i;
+                break;
+            }
+        }
+        for(int i:ans){
+            System.out.print(i+" ");
+        }
+    }
+}
